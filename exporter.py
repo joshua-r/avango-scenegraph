1b5c2bce671fcc88c71b1f3f6e01894e29ec8d73
Joshua Reibert <joshua.reibert@uni-weimar.de>
2017-03-25 17:00:53 +0100

    Export all fields by name instead of directly specifying those to be exported
    Fields can be blacklisted to be excluded.

--- exporter.py
+++ exporter.py
@@ -6,28 +6,58 @@ import json
 
 from collections import deque
 
+FIELD_BLACKLIST = [
+    'BoundingBox',
+    'Children',
+    'Parent',
+    'Path',
+    'Transform',
+    'WorldTransform'
+]
 
-def vec3_to_list(vec):
-    '''Converts an avango-gua Vec3 to a list of its components'''
-    return [vec.x, vec.y, vec.z]
 
-
-def quat_to_list(quat):
-    '''Converts an avango-gua Quaternion to a list containing x, y, z and w'''
-    return [quat.x, quat.y, quat.z, quat.w]
+def to_json_type(var):
+    if type(var) in [str, int, float, bool]:
+        return var
+    elif type(var) == avango.gua.Vec3:
+        return [var.x, var.y, var.z]
+    elif type(var) == avango.gua.Vec4:
+        return [var.x, var.y, var.z, var.w]
+    elif type(var) == avango.gua.Quat:
+        return [var.x, var.y, var.z, var.w]
+    elif type(var) == avango.gua.Color:
+        return [var.r, var.g, var.b]
+    elif type(var) == avango.gua.Material:
+        return {
+            'Name': var.Name.value,
+            'ShaderName': var.ShaderName.value,
+            'EnableBackfaceCulling': var.EnableBackfaceCulling.value
+            }
+    elif type(var) in [avango._avango.MFString_wrapper,
+                       avango._avango.MFFloat_wrapper]:
+        return [item for item in var]
+    else:
+        raise TypeError(
+            'There is no conversion from type {} to a json-compatible type'
+                .format(type(var).__name__))
 
 
 def node_to_dict(node, id, parent_id):
     '''Converts an avango-gua node to a python dictionary'''
-    return {
-        'name': node.Name.value,
-        'trans': vec3_to_list(node.Transform.value.get_translate()),
-        'rot': quat_to_list(node.Transform.value.get_rotate_scale_corrected()),
-        'scale': vec3_to_list(node.Transform.value.get_scale()),
-        'type': type(node).__name__,
-        'id': id,
-        'parent': parent_id,
+    d = {
+        'ID': id,
+        'Parent': parent_id,
+        'Type': type(node).__name__,
+        'Trans': to_json_type(node.Transform.value.get_translate()),
+        'Rot': to_json_type(node.Transform.value.get_rotate_scale_corrected()),
+        'Scale': to_json_type(node.Transform.value.get_scale()),
     }
+    for i in range(node.get_num_fields()):
+        field_name = node.get_field_name(i)
+        if not field_name in FIELD_BLACKLIST:
+            d[field_name] = to_json_type(node.get_field(i).value)
+
+    return d
 
 
 def export_scenegraph(graph, filename,
