0adb7a5416eb2b884092dd56aebdd674bdabab46
Joshua Reibert <joshua.reibert@uni-weimar.de>
2017-05-27 13:42:54 +0200

    Use queue, exclude root node and create it separately

--- export.py
+++ export.py
@@ -6,16 +6,26 @@ import json
 from .conversion import node_to_dict
 
 
-def export_scenegraph(graph, filename,
+def export_scenegraph(graph,
+                      filename,
                       type_blacklist=['CameraNode', 'ScreenNode']):
     '''Writes the given avango-gua scene graph into a json-file'''
 
-    # counter as unique id for every node to store relations between nodes
-    node_id = 0
+    # counter as unique id for every node to store relations between nodes;
+    # starts at one because root node is 0
+    node_id = 1
 
     # holds tuples of node and parent-id for nodes to be stored
-    # initialized with root node that has no parent
-    queue = deque([(graph.Root.value, -1)])
+    queue = deque()
+
+    # function to add tuples of the form (node, parent_id) to the queue
+    def add_nodes_to_queue(nodes):
+        queue.extend([(node, parent) for node, parent in nodes
+                      if not type(node).__name__ in type_blacklist])
+
+    # initialize queue with child nodes of the root node
+    add_nodes_to_queue([(child, 0)
+                        for child in graph.Root.value.Children.value])
 
     # store graph as json into file with of given filename
     with open(filename, 'w') as json_file:
@@ -24,16 +34,17 @@ def export_scenegraph(graph, filename,
             node, parent_id = queue.popleft()
 
             # write node as one json-object as one line into the file
-            json_file.write(json.dumps(
-                obj=node_to_dict(node=node, id=node_id, parent_id=parent_id),
-                sort_keys=True))
+            json_file.write(
+                json.dumps(
+                    obj=node_to_dict(
+                        node=node, id=node_id, parent_id=parent_id),
+                    sort_keys=True))
             json_file.write('\n')
 
             # add the node's children to the queue and store the current node's
             # id as their parent-id
-            queue.extend([(child, node_id)
-                          for child in node.Children.value
-                          if not type(child).__name__ in type_blacklist])
+            add_nodes_to_queue([(child, node_id)
+                                for child in node.Children.value])
 
             # increase the counter to assign the next node the next free id
             node_id += 1
--- graph_import.py
+++ graph_import.py
@@ -13,7 +13,7 @@ def import_scenegraph(filename, name='scenegraph',
     '''Reads a previously exported avango-gua scene graph from a json-file'''
     graph = avango.gua.nodes.SceneGraph(Name=name)
 
-    # dictionary to map node ids to actual nodes
+    # dictionary to map node ids to actual nodes; fill initially with root
     nodes = {0: graph.Root.value}
 
     # load graph from json of given file name
@@ -22,15 +22,9 @@ def import_scenegraph(filename, name='scenegraph',
             # parse json line to node
             node, id, parent_id = dict_to_node(json.loads(line))
 
-            # restore hierarchy based on the loaded parent id
-            if parent_id == -1:
-                # -1 marks the root node
-                pass
-                # TODO: set attributes that are allowed to be written
-            else:
-                # store node by id
-                nodes[id] = node
-                # append to its parent
-                nodes[parent_id].Children.value.append(node)
+            # store node by id
+            nodes[id] = node
+            # append to its parent
+            nodes[parent_id].Children.value.append(node)
 
     return graph
